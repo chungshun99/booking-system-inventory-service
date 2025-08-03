963d555710af7f228edb70d60a20f76c2a8ee4d6
chungshun99 <chungshun99@gmail.com>
2025-08-03 22:48:18 +0800

    First commit

---
+++ src/main/java/com/project/inventory_service/services/InventoryService.java
@@ -0,0 +1,70 @@
+package com.project.inventory_service.services;
+
+import com.project.inventory_service.entity.Event;
+import com.project.inventory_service.entity.Venue;
+import com.project.inventory_service.repository.EventRepository;
+import com.project.inventory_service.repository.VenueRepository;
+import com.project.inventory_service.response.EventInventoryResponse;
+import com.project.inventory_service.response.VenueInventoryResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class InventoryService {
+
+    private final EventRepository eventRepository;
+    private final VenueRepository venueRepository;
+
+    @Autowired
+    public InventoryService(final EventRepository eventRepository, final VenueRepository venueRepository) {
+        this.eventRepository = eventRepository;
+        this.venueRepository = venueRepository;
+    }
+
+
+    public List<EventInventoryResponse> getAllEvents() {
+        final List<Event> events = eventRepository.findAll();
+
+        return events.stream().map(event -> EventInventoryResponse.builder()
+                .event(event.getName())
+                .capacity(event.getLeftCapacity())
+                .venue(event.getVenue())
+                .build()).collect(Collectors.toList());
+
+    }
+
+    public VenueInventoryResponse getVenueInformation(Long venueId) {
+        final Venue venue = venueRepository.findById(venueId).orElse(null);
+
+        return VenueInventoryResponse.builder()
+                .venueId(venue.getId())
+                .venueName(venue.getName())
+                .totalCapacity(venue.getTotalCapacity())
+                 .build();
+    }
+
+    public EventInventoryResponse getEventInventory(final Long eventId) {
+        final Event event = eventRepository.findById(eventId).orElse(null);
+
+        return EventInventoryResponse.builder()
+                .event(event.getName())
+                .capacity(event.getLeftCapacity())
+                .venue(event.getVenue())
+                .ticketPrice(event.getTicketPrice())
+                .eventId(event.getId())
+                .build();
+    }
+
+
+    public void updateEventCapacity(final Long eventId, final Long ticketsBooked) {
+        final Event event = eventRepository.findById(eventId).orElse(null);
+        event.setLeftCapacity(event.getLeftCapacity() - ticketsBooked);
+        eventRepository.saveAndFlush(event);
+        log.info("Updated event capacity for event id: {} with tickets booked: {}", eventId, ticketsBooked);
+    }
+}
---
+++ src/main/resources/db/migration/V3__create_customer_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customer (
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    address varchar(255) NOT NULL
+);
