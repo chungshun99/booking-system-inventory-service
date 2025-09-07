37810e52d06a0a9ebb2be59ca9cb4e1b89e7e6d4
chungshun99 <chungshun99@gmail.com>
2025-09-08 01:53:05 +0800

    First commit

--- src/main/java/com/project/inventory_service/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.inventory_service.repository;
-
-import com.project.inventory_service.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- src/main/java/com/project/inventory_service/repository/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.inventory_service.repository;
-
-import com.project.inventory_service.entity.Venue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-}
